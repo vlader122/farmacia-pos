55452155ac52b1126b78497c74f4e3ebdf9784f4
niver.copa <vlader122@gmail.com>
2025-03-10 22:33:28 -0400

    clase 3

---
+++ DB/PosFarmaciaContext.cs
@@ -0,0 +1,17 @@
+﻿using DB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class PosFarmaciaContext: DbContext
+    {
+        public PosFarmaciaContext(DbContextOptions<PosFarmaciaContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ PosFarmacia/Program.cs
@@ -0,0 +1,36 @@
+using DB;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Conectar con la BD
+builder.Services.AddDbContext<PosFarmaciaContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PosFarmaciaConeccion"));
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<PosFarmaciaContext>();
+    context.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
